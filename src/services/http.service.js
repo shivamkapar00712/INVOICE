635eb7073241b0b3e6a060fb90d26c1b36cb137c
shivamKapar <shivam.Kapar@applore.in>
2022-12-22 23:14:19 +0530

    fixed bug

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = "http://localhost:8081/api";
+axios.defaults.baseURL = "https://invoice-node.onrender.com";
 
 function setJWT() {
   axios.defaults.headers.common["authorization"] =
